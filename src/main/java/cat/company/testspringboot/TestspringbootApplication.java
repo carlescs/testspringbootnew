b542ee47bdda7b39dfa140a19e1d1c82e051fcc5
Carles Company Soler <carlescs@gmail.com>
2023-09-01 18:39:33 +0200

    first commit

---
+++ src/main/java/cat/company/testspringboot/TestspringbootApplication.java
@@ -0,0 +1,12 @@
+package cat.company.testspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestspringbootApplication.class, args);
+	}
+}
