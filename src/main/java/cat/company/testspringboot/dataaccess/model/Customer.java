3db23e457be8ba9ab207614855bbfd5b40c5395c
Carles Company Soler <carlescs@gmail.com>
2023-09-04 20:58:51 +0200

    Switch to using sequence for id generation

--- src/main/java/cat/company/testspringboot/dataaccess/model/Customer.java
+++ src/main/java/cat/company/testspringboot/dataaccess/model/Customer.java
@@ -4,12 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 
 @Entity
 public class Customer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_id_seq")
+    @SequenceGenerator(name = "customer_id_seq", sequenceName = "customer_id_seq", allocationSize = 1)
     private long id;
     private String firstName;
     private String lastName;
