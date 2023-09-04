64115f1a23707156c38af669b8064bd1130431b5
Carles Company Soler <carlescs@gmail.com>
2023-09-04 17:08:48 +0200

    Add get one. Fix Post.

--- src/main/java/cat/company/testspringboot/controllers/HelloController.java
+++ src/main/java/cat/company/testspringboot/controllers/HelloController.java
@@ -3,9 +3,13 @@ package cat.company.testspringboot.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import cat.company.testspringboot.dataaccess.model.Customer;
 import cat.company.testspringboot.dataaccess.repositories.CustomerRepository;
@@ -17,12 +21,18 @@ public class HelloController {
     private CustomerRepository repository;
 
     @GetMapping("/")
-    public List<Customer> index(){
+    public List<Customer> index() {
         return repository.findAll();
     }
 
+    @GetMapping("/customer/{id}")
+    public Customer getCustomer(@PathVariable("id") Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Customer not found"));
+    }
+
     @PostMapping("/customer")
-    public Customer createCustomer(Customer customer){
+    public Customer createCustomer(@RequestBody Customer customer) {
         return repository.save(customer);
     }
 }
