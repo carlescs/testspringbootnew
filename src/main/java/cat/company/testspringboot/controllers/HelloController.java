d32bdc46efdec35a1b1504ca388d5913471ec9e6
Carles Company Soler <carlescs@gmail.com>
2023-09-04 17:27:33 +0200

    Add some http methods.

--- src/main/java/cat/company/testspringboot/controllers/HelloController.java
+++ src/main/java/cat/company/testspringboot/controllers/HelloController.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -15,24 +18,39 @@ import cat.company.testspringboot.dataaccess.model.Customer;
 import cat.company.testspringboot.dataaccess.repositories.CustomerRepository;
 
 @RestController
+@RequestMapping("/api/customers")
 public class HelloController {
 
     @Autowired
     private CustomerRepository repository;
 
-    @GetMapping("/")
+    @GetMapping("")
     public List<Customer> index() {
         return repository.findAll();
     }
 
-    @GetMapping("/customer/{id}")
+    @GetMapping("{id}")
     public Customer getCustomer(@PathVariable("id") Long id) {
         return repository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Customer not found"));
     }
 
-    @PostMapping("/customer")
+    @PostMapping("")
     public Customer createCustomer(@RequestBody Customer customer) {
         return repository.save(customer);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteCustomer(@PathVariable("id") Long id) {
+        repository.deleteById(id);
+    }
+
+    @PutMapping("{id}")
+    public Customer updateCustomer(@PathVariable("id") Long id, @RequestBody Customer customer) {
+        Customer customerToUpdate = repository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Customer not found"));
+        customerToUpdate.setFirstName(customer.getFirstName());
+        customerToUpdate.setLastName(customer.getLastName());
+        return repository.save(customerToUpdate);
     }
 }
--- src/main/java/cat/company/testspringboot/dataaccess/model/Customer.java
+++ src/main/java/cat/company/testspringboot/dataaccess/model/Customer.java
@@ -40,4 +40,12 @@ public class Customer {
     public String getLastName() {
         return lastName;
     }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
